a67d6d364c4020eee56189e4a56abcf33b7c2df5
saraWilhelem <Hand4d11@mbjcomp.org.il>
2024-11-04 15:57:37 +0200

    this is stage0

---
+++ stage0/Program1288.cs
@@ -0,0 +1,17 @@
+﻿partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Welcome1288();
+        Welcome6678();
+    }
+
+    private static void Welcome1288()
+    {
+        Console.WriteLine("Enter your name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("{0}, welcome to my first console application", name);
+    }
+    private static void Welcome6678()
+    { }
+}
